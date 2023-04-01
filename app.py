3d4959304a1a827e4f930fc28485879607378f6c
foco22 <francisco.macaya22@gmail.com>
2023-04-01 02:58:56 -0300

    new features

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ from fastapi.responses import FileResponse
 import os
 from random import randint
 import uuid
-import Modelo as modelo
+import Model as model
 import uvicorn
 from PIL import Image
 from fastapi.responses import JSONResponse
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from fastapi.encoders import jsonable_encoder
-
-
-
-###################### API #########################################
 
 app = FastAPI()
 
@@ -26,18 +22,18 @@ def create_upload_file(file: UploadFile = File(None)):
     
     #print(file[0])
 
-    tipo_file = ['.jpeg', '.jpg'] 
+    type_file = ['.jpeg', '.jpg'] 
     
-    conteo_file = 0
-    for j in tipo_file:
+    count_file = 0
+    for j in type_file:
         if j not in  file.filename: 
-          conteo_file = conteo_file + 1
+          count_file = count_file + 1
     
-    if conteo_file == len(tipo_file):
+    if count_file == len(type_file):
        return  HTTPException(status_code = 404, detail=  "File must have a JPEG or JPG extension")
           
 
-    modelo_resultados = modelo.detecion_face(file.file)
+    modelo_resultados = model.face_detection(file.file)
 
     if 'My face appears in the picture' == modelo_resultados:
       return JSONResponse(status_code=200, content={"message": "My face appears in the picture"})
@@ -48,9 +44,11 @@ def create_upload_file(file: UploadFile = File(None)):
     elif 'The picture does not contain any faces' == modelo_resultados:
       return JSONResponse(status_code=200, content={"message": 'The picture does not contain any faces'})
 
+
 @app.exception_handler(500)
 async def internal_exception_handler(request: Request, exc: Exception):
   return JSONResponse(status_code=500, content=jsonable_encoder({"code": 500, "msg": "Internal Server Error"}))
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0')
---
+++ constants.py
@@ -0,0 +1,5 @@
+thresh=0.2
+picture = 'picture.jpeg'
+input_shape_x = 600
+input_shape_y = 600
+input_shape_z = 3
